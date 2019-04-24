06fa55a601aae65d7a4c61a1e6f293786a3e9245
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2019-04-24 23:56:24 +0900

    routeを動的route２つにして対応させた

---
+++ pages/show/_userName/_repository.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <div>
+      <ul>
+        <li v-for="item in items.owner" :key="item">
+          <h1>{{ item }}</h1>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  async asyncData({ route, app }) {
+    const items = await app.$axios.$get(`https://api.github.com/repos/${route.params.userName}/${route.params.repository}`)
+    return { items }
+  }
+}
+</script>
